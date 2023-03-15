0188326cdbc83d10867197996d38e078f03f5407
Reza Afsharnia <s.reza.afsharnia@gmail.com>
2023-03-15 12:30:09 +0330

    send code improved

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const users = require("./users.js");
 const bodyParser = require("body-parser");
 const app = express();
 const port = 5000;
 
 //
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  res.setHeader("Access-Control-Allow-Methods", "*");
-  next();
-});
+
 //
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -19,18 +16,14 @@ app.use(bodyParser.json());
 // const MelipayamakApi = require("melipayamak-api");
 
 const { Smsir } = require("smsir-js");
-/**
- *
- * @param {string} api_key
- * @param {int} line_number
- */
+
 const smsir = new Smsir(
   "lOb7CFVigvBGc9wqhK23thEBLm4d9h64xXYmwscJSrFP87aApcNwvip0fRAK8XsB",
   "30007732004112"
 );
 
 //
-
+app.options("*", cors());
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 app.post("/sendOtp", (req, res) => {
@@ -38,6 +31,10 @@ app.post("/sendOtp", (req, res) => {
   smsir.SendVerifyCode(`${req.body.phone}`, 182711, [
     { name: "CODE", value: `${value}` },
   ]);
-  res.send()
+  res.status(200).json({
+    data: {
+      value: value,
+    },
+  });
   console.log(req.body);
 });
